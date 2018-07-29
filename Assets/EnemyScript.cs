0a328760129b70d1cbd652bbe320b75212924fd9
Sixsrs <sixers27.72@gmail.com>
2018-07-29 19:50:39 -0400

    Fighting
    - Bullets
    - Enemies
    - FixedUpdate
    - +5 Jump
    - Triggers for bullets and Enemies

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+
+    public float enemyHealth;
+	// Use this for initialization
+	void Start () {
+        enemyHealth = 1f;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		if (enemyHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("PlayerStandardBulletTrigger")) 
+            {
+            enemyHealth = enemyHealth - 1;
+        }
+    }
+}
--- Assets/PlayerScript.cs
+++ Assets/PlayerScript.cs
@@ -5,29 +5,75 @@ using UnityEngine;
 public class PlayerScript : MonoBehaviour {
 
     private Rigidbody2D rb;
+
+    public float playerHealth;
     public float jumpSpeed;
+
     public float groundCheckRadius;
     public LayerMask whatIsGround;
     private bool onGround;
 
-	// Use this for initialization
-	void Start () {
+    public GameObject Projectile;
+    public float bulletCoolDown = 0f;
+    public float bulletMaxCoolDown;
+    public bool bulletOnCoolDown = false;
 
+    public float hiddenTimer;
+    public float secondTimer;
+
+
+
+    // Use this for initialization
+    void Start () {
+
+        playerHealth = 1f;
         rb = GetComponent<Rigidbody2D>();
+        bulletMaxCoolDown = 25f;
+       
 
-	}
+    }
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
+        if (playerHealth < 1)
+        {
+            Destroy(gameObject);
+        }
+
+        if (bulletCoolDown < 1f)
+        {
+            bulletOnCoolDown = false;
+        }
+        else
+        {
+            bulletCoolDown--;
+        }
+
+
         float below_distance = 1.2f;  // radius of player plus a little         Vector3 down = new Vector3(transform.position.x, transform.position.y - below_distance, 0.0f);
         onGround = Physics2D.OverlapCircle(down, groundCheckRadius, whatIsGround);
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0.0f, 0.0f);
         transform.position = transform.position + move * .13f;
-        if (Input.GetKeyDown(KeyCode.Space)&& onGround||Input.GetKeyDown(KeyCode.W)&& onGround){
+        if (Input.GetKeyDown(KeyCode.W)&& onGround||Input.GetKeyDown(KeyCode.W)&& onGround){
             rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && bulletOnCoolDown == false)
+        {
+            Instantiate(Projectile, new Vector3(transform.position.x, transform.position.y, 0f), Quaternion.identity);
+            bulletOnCoolDown = true;
+            bulletCoolDown = bulletMaxCoolDown;
+
+
         }
         if (Input.GetKey(KeyCode.S)&& onGround==false){
             rb.velocity = new Vector2(rb.velocity.x, -jumpSpeed);
         }
 	}
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("EnemyTrigger"))
+        {
+            playerHealth = playerHealth - 1;
+        }
+    }
 }
